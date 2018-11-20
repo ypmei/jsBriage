50b5c0eae99baf2adcccf6757a1e0bdfb30262d1
danielwu <ypmei_money@git.cloud.tencent.com>
2018-11-20 11:51:19 +0800

    Initial Commit

---
+++ webtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webtest
+//
+//  Created by ypmei on 2018/11/20.
+//  Copyright © 2018年 ypmei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
